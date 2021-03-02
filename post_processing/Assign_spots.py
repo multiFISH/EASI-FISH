8c78dd7370d482e41c92f0d0ad936b4b786d6afe
Yuhan Wang <wangyuhan01@gmail.com>
2021-03-01 22:01:21 -0500

    update assign_spots.py

--- post_processing/Assign_spots.py
+++ post_processing/Assign_spots.py
@@ -1,41 +1,49 @@
-# Code for assigning spot counts to neurons. 
-# Spots instead of neuron IDs are queried for rapid processing of large datasets. 
 
-import os, sys
+import os
+import sys
 import numpy as np
 import pandas as pd
-from glob import glob 
+from glob import glob
 from skimage.io import imread
 from os.path import abspath, dirname
 
 if __name__ == '__main__':
-    lb_dir         = sys.argv[1]  # directory to the segmentation mask (tif format accepted here)
-    spot_dir       = sys.argv[2]  # directory to folder of detected spot (csv format, usually Airlocalize output)
-    out_dir        = sys.argv[3]  # directory where the output file should be saved 
-    
-lb=imread(lb_dir)
-fx=sorted(glob(spot_dir+"/*.txt"))
+    lb_dir = sys.argv[1]
+    spot_dir = sys.argv[2]
+    out_dir = sys.argv[3]
 
-lb_id=np.unique(lb[lb!=0])
-z,y,x=lb.shape
+lb = imread(lb_dir)
+fx = sorted(glob(spot_dir+"/*.txt"))
+
+lb_id = np.unique(lb[lb != 0])
+z, y, x = lb.shape
 s=[0.92,0.92,0.84] ## voxel size in segmentation image
-
-count=pd.DataFrame(np.empty([len(lb_id), 0]), index=lb_id)
+count = pd.DataFrame(np.empty([len(lb_id), 0]), index=lb_id)
 
 for f in fx:
-    r=os.path.basename(f).split('/')[-1]
-    r=r.split('.')[0]
-    spot=np.loadtxt(f, delimiter=',')
+    print("Load:", f)
+    r = os.path.basename(f).split('/')[-1]
+    r = r.split('.')[0]
+    spot = np.loadtxt(f, delimiter=',')
     n = len(spot)
-    spot[:,:3] = spot[:,:3]/s
-    spot= np.round(spot).astype('int')
-    df=pd.DataFrame(np.zeros([len(lb_id),1]),index=lb_id,columns=['count'])
-    
-    for i in range(0,n):
-        Coord = np.minimum(spot[i,:3],[x,y,z])
-        idx = lb[Coord[2]-1,Coord[1]-1,Coord[0]-1]
-        if idx>0:
-            df.loc[idx,'count']= df.loc[idx,'count']+1
-    count.loc[:,r]=df.to_numpy()
-count.to_csv(out_dir+'/count.csv')
+    rounded_spot = np.round(spot[:, :3]/s).astype('int')
+    df = pd.DataFrame(np.zeros([len(lb_id), 1]),
+                      index=lb_id, columns=['count'])
 
+    for i in range(0, n):
+        if np.any(np.isnan(spot[i,:3])):
+            print('NaN found in {} line# {}'.format(f, i+1))  
+        else:
+            if np.any(spot[i,:3]<0):
+                print('Point outside of fixed image found in {} line# {}'.format(f, i+1)) 
+            else: 
+                try:
+                    # if all non-rounded coord are valid values (none is NaN)
+                    Coord = np.minimum(rounded_spot[i], [x, y, z])
+                    idx = lb[Coord[2]-1, Coord[1]-1, Coord[0]-1]
+                    if idx > 0 and idx <= len(lb_id):
+                        df.loc[idx, 'count'] = df.loc[idx, 'count']+1
+                except Exception as e:
+                    print('Unexpected error in {} line# {}: {}'.format(f, i+1, e))
+    count.loc[:, r] = df.to_numpy()
+count.to_csv(out_dir+'/count.csv')
